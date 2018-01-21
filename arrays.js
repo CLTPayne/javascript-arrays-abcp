9a6313eb7260e612bd9278b4dfa104acfae0dcb1
Charlotte Payne <cltpayne@gmail.com>
2018-01-21 23:02:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,8 @@ var chocolateBars = [
   ]
   
 function addElementToBeginningOfArray(array, element) {
-  
+  var thisArray = ["array"]
+  thisArray.push("element")
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
